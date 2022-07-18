6742b4b9089b331fc7ab268e184f0a39d4ffddd7
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-18 02:37:38 +0100

    full authentication

--- expenseswebsite/authentication/urls.py
+++ expenseswebsite/authentication/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 urlpatterns = [
     path("register", RegistrationView.as_view(), name="register"),
     path("login", LoginView.as_view(), name="login"),
+    path("logout", LogoutView.as_view(), name="logout"),
     path("validate-username", csrf_exempt(UsernameValidationView.as_view()),
          name="validate-username"),
     path("validate-email", csrf_exempt(EmailValidationView.as_view()),
