0f64fa4c1d04c97443c7c4851585f89367b3e974
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-22 10:28:39 +0100

    pagination

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from . models import *
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -10,9 +11,12 @@ from django.contrib import messages
 def index(request):
     categories = Category.objects.all()
     expenses = Expense.objects.filter(owner=request.user)
-
+    paginator = Paginator(expenses, 2)
+    page_number = request.GET.get("page")
+    page_obj = Paginator.get_page(paginator, page_number)
     context = {
         "expenses": expenses,
+        "page_obj": page_obj,
     }
 
     return render(request, "expenses/index.html", context)
