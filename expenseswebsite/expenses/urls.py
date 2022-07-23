52cf111fd4b89f84051e6e577c5855b11dc02a8e
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-23 01:49:50 +0100

    search expense field bug fix

--- expenseswebsite/expenses/urls.py
+++ expenseswebsite/expenses/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
@@ -7,5 +8,6 @@ urlpatterns = [
     path("add-expense", views.add_expense, name="add-expenses"),
     path("edit-expense/<int:id>", views.expense_edit, name="expense-edit"),
     path("expense-delete/<int:id>", views.delete_expense, name="expense-delete"),
-    path("search-expenses", views.search_expenses, name="search_expenses"),
+    path("search-expenses", csrf_exempt(views.search_expenses),
+         name="search_expenses"),
 ]
