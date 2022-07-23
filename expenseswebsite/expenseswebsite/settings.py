d19577ecfc2ce95d4b1aa81dee4edaa97e81cddb
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-23 04:19:38 +0100

    income app in django added

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     "expenses",
     "authentication",
     "userpreferences",
+    "userincome",
 ]
 
 MIDDLEWARE = [
--- expenseswebsite/expenseswebsite/urls.py
+++ expenseswebsite/expenseswebsite/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path("authentication/", include("authentication.urls")),
     path("", include("expenses.urls")),
     path("preferences/", include("userpreferences.urls")),
+    path('income/', include('userincome.urls')),
     path('admin/', admin.site.urls),
 ]
