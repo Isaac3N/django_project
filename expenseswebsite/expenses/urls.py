40d5a86237e1b693823e9a3ab774f006165b76b0
Isaac Ndubuisi <theisaac3n@gmail.com>
2022-07-24 13:20:01 +0100

    data visualization with chartjs

--- expenseswebsite/expenses/urls.py
+++ expenseswebsite/expenses/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path("expense-delete/<int:id>", views.delete_expense, name="expense-delete"),
     path("search-expenses", csrf_exempt(views.search_expenses),
          name="search_expenses"),
+    path("expense_category_summary", views.expense_category_summary,
+         name='expense_category_summary'),
+    path("stats", views.stats_view, name="stats")
 ]
